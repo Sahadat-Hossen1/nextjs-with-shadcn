7cd6b6e0bd362e9b643791fe537adf6b5fa4b949
Sahadat-Hossen1 <sahadat.hossen1435@gmail.com>
2026-05-07 13:22:45 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>
+        this is home page
+      </h1>
+      {/* nave   */}
+      {/* header  */}
+      {/* main  */}
+      
+      {/* content  */}
+      {/* footer  */}
+    </div>
+  );
+}

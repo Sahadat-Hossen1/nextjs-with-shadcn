a0de99582e4e785fd05aa29b013622efdbbfeb40
Sahadat-Hossen1 <sahadat.hossen1435@gmail.com>
2026-05-09 00:39:45 +0600

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,35 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
     <div>
-      <h1>
-        this is home page
-      </h1>
-      {/* nave   */}
-      {/* header  */}
-      {/* main  */}
-      
-      {/* content  */}
-      {/* footer  */}
+      <h1>this is home page</h1>
+      {/* add shadcn button */}
+      <Button variant="outline">shadcn button</Button>
+      {/*  */}
+      <Button variant="default">Default</Button>
+
+      <Button variant="destructive">Delete</Button>
+
+      <Button variant="outline">Outline</Button>
+
+      <Button variant="secondary">Secondary</Button>
+
+      <Button variant="ghost">Ghost</Button>
+
+      <Button variant="link">Link</Button>
+      {/*  */}
+      <Button size="sm">Small</Button>
+
+      <Button size="default">Normal</Button>
+
+      <Button size="lg">Large</Button>
+
+      <Button size="icon">+</Button>
+      {/*  */}
+      <Button className="bg-red-500 hover:bg-red-700 text-white rounded-full px-8">
+        Custom Button
+      </Button>
     </div>
   );
 }
